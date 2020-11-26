e80aa57485f66e150c4306c346a750f6a7ac9261
Nick Payne <nick@nickpayne. net>
2020-11-26 19:13:30 +1100

    Get Eventbrite events for Workshops page

---
+++ __js/more/eventbrite.js
@@ -0,0 +1,28 @@
+// Get upcoming workshops from meetup
+$(function() {
+  eventbrite_url = 'https://www.eventbriteapi.com/v3/organizations/266418941811/events/?status=live&token=HS4VPDV6QYZK3J7B5BS3';
+  $.getJSON(eventbrite_url, function(response) {
+    $.getJSON()
+    var tbody = $('#workshops tbody');
+    for(var event in response.events) {
+      var startDate = moment(response.events[event].start.local);
+      var endDate = moment(response.events[event].end.local);
+      tbody.append(
+        '<tr>' +
+          '<td>' + 
+            startDate.format('dddd, MMMM Do YYYY') + '<br>' +
+            startDate.format('h:mmA') + ' - ' + endDate.format('h:mmA') +
+          '</td>' +
+          '<td>' + 
+            '<dl>' +
+              '<dt>' + response.events[event].name.text + '</dt>' +
+              '<dd>' +
+                '<a href="' + response.events[event].url + '" target="_blank">Get tickets</a>' +
+              '</dd>' +
+            '</dl>' +
+          '</td>' +
+        '</tr>'
+      );
+    }
+  });
+});
