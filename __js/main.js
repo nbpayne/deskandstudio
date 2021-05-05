722703094a2f55c9118df5c6cbf009d3c84fd439
Nick Payne <nick@nickpayne. net>
2021-05-05 21:21:46 +1000

    Ditch FormSpree and try FormSpark
    From the spree to the spark. Also, added new fields.

--- __js/main.js
+++ __js/main.js
@@ -1,14 +1,3 @@
 console.log('Hello Petersham.');
 
 var myLazyLoad = new LazyLoad();
-
-/*
-$(document).ready(function() {
-  if (window.location.pathname === '/' && sessionStorage.modalShown != 1) {
-    sessionStorage.modalShown = 1;
-    setTimeout(function() {
-      $('.modal').modal('show');
-    }, 1000);
-  }
-});
-*/
