1ef6419be8044c17c9323a1b3e6bed4870c4d3d2
Nick Payne <nick@nickpayne. net>
2022-01-18 09:53:20 +1100

    Turn modal off

--- __js/main.js
+++ __js/main.js
@@ -2,11 +2,11 @@ console.log('Hello Petersham.');
 
 var myLazyLoad = new LazyLoad();
 
-$(document).ready(function() {
-  if (sessionStorage.modalShown != 1) {
-    sessionStorage.modalShown = 1;
-    setTimeout(function() {
-      $('.modal').modal('show');
-    }, 1000);
-  }
-});
+// $(document).ready(function() {
+//   if (sessionStorage.modalShown != 1) {
+//     sessionStorage.modalShown = 1;
+//     setTimeout(function() {
+//       $('.modal').modal('show');
+//     }, 1000);
+//   }
+// });
